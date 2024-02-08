6bf99ef8e5598c8c7b8efbd439c6ca0707cafd69
Letícia <leticia_vanelli@estudante.sesisenai.org.br>
2024-02-08 11:06:46 -0300

    adicionado a classe e os estilos

--- voto_main.js
+++ voto_main.js
@@ -6,5 +6,14 @@ verificar.addEventListener(`click`, ()=>{ //ou function
     let idade = Number(document.getElementById('idade').value)
     let titulo = Number(document.getElementById('titulo').value)
 
-    console.log(nome,idade,titulo)
+    console.log("dados doformulário", nome,idade,titulo)
+
+    const cidadao = new Eleitor()
+
+    cidadao.nome = nome
+    cidadao.idade = idade
+    cidadao .titulo = titulo
+
+    console.log("instância do objeto" ,cidadao)
+
 })
